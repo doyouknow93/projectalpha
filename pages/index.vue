4fce14eb95facecf5bb4a41cdd629767da3eebed
doyouknow93 <pierri93@gmail.com>
2022-10-04 00:47:14 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div v-for="result in results" :key="result.symbol">
+      {{result.symbol}}
+    </div>
+    Welcome to Mammeta
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data() {
+      return {
+      results: null
+    }
+  }
+    ,
+    head() {
+        return {
+            title: 'Welcome to Dexter',
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: 'Dexter Home'
+                }
+            ]
+        };
+    },
+    
+    async mounted () {
+      console.log("i'm created");
+      const res= await axios.get('https://api2.binance.com/api/v3/ticker/24hr')
+      console.log(res.data);
+      this.results=res.data;
+    }
+
+
+};
+</script>
