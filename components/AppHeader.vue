09a9df7b7f775854d26f51f315c8f9fc73dbe82c
doyouknow93 <pierri93@gmail.com>
2022-10-04 03:59:05 +0200

    .

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="header">
-      <h1 class="title">Dad Jokes</h1>
+      <h1 class="title">Dexter</h1>
       <ul>
         <li>
           <nuxt-link to="/">Home</nuxt-link>
