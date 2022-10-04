f17794ce93a329777f89069026a11999b7b367e5
egeominotti <egeominotti@gmail.com>
2022-10-04 15:55:54 +0200

    - test

--- pages/Prices/index.vue
+++ pages/Prices/index.vue
@@ -9,37 +9,35 @@ import axios from 'axios';
 import Price from '../../components/Price';
 
 export default {
-    data() {
-        return {
-            prices: []
-        };
-    },
-    async created() {
-        const config = {
-            headers: {
-            }
-        };
-        try {
-            const res = await axios.get("https://api2.binance.com/api/v3/ticker/24hr", config);
-            this.prices = res.data.result;
-            console.log(res.data);
+  data() {
+    return {
+      prices: []
+    };
+  },
+  async created() {
+    const config = {
+      headers: {}
+    };
+    try {
+      const res = await axios.get("https://api2.binance.com/api/v3/ticker/24hr", config);
+      this.prices = res.data.result;
+      console.log(res.data);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  head() {
+    return {
+      title: "Prices",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content: "Prices"
         }
-        catch (err) {
-            console.log(err);
-        }
-    },
-    head() {
-        return {
-            title: "Prices",
-            meta: [
-                {
-                    hid: "description",
-                    name: "description",
-                    content: "Prices"
-                }
-            ]
-        };
-    },
-    components: { Price }
+      ]
+    };
+  },
+  components: {Price}
 };
 </script>
